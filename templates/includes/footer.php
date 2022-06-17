c744409afde09f28435c87b3461f0ca4ee319177
juanca-dev <juancarlos17.mf@gmail.com>
2022-06-17 12:17:33 -0500

    first commit

---
+++ templates/includes/footer.php
@@ -0,0 +1,18 @@
+  <!-- footer -->
+  <footer class="bg-light text-muted py-5">
+    <div class="container">
+      <p class="float-right">
+        <a href="#">Arriba</a>
+      </p>
+      <p><a href="https://getbootstrap.com/">Cotizador App</a> &copy;. Todos los derechos reservados <?php echo date('Y'); ?>.</p>
+    </div>
+  </footer>
+  <!-- ends footer -->
+
+  <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  <script src="<?php echo JS.'waitMe.min.js' ?>"></script>
+  <script src="<?php echo JS.'main.js'; ?>"></script>
+</body>
+</html>
---
+++ templates/includes/head.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php echo APP_NAME; ?></title>
+
+  <!-- Estilos bootstrap 4 -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?php echo JS.'waitMe.min.css'; ?>">
+</head>
+<body>
